6fbe1d537be84c7f3b971423dd68343ad31a6f7d
Dmitriy Fateyev <dmitriy.fateyev86@gmail.com>
2020-05-23 12:17:17 +0400

    homework6

---
+++ Lesson6/homework6/task2/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.IO;
+
+// Студент: Дмитрий Фатеев
+
+/* Задание №2
+ * Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+ * а) Сделать меню с различными функциями и представить пользователю выбор, для какой функции и на каком отрезке находить
+ * минимум. Использовать массив (или список) делегатов, в котором хранятся различные функции.
+ * б) (*)Переделать функцию Load, чтобы она возвращала массив считанных значений. Пусть она возвращает минимум функции через 
+ * параметр (с использованием модификатора out). 
+ */
+
+
+namespace DoubleBinary
+{
+    public delegate double Fn(double x);
+
+    class Program
+    {
+        public static double F1(double x)
+        {
+            return x * x - 50 * x - 10; // x^2-50*x-10
+        }
+
+        public static double F2(double x)
+        {
+            return x * x + 4 * x - x;   // x^2+4*x-x
+        }
+
+        public static double F3(double x)
+        {
+            return x * x - Math.Cos(x); // x^2-cos(x)
+        }
+
+        
+
+        // Модифицировать программу нахождения минимума функции так, чтобы можно было передавать функцию в виде делегата.
+        public static void SaveFunc(string fileName, Fn delegFunc, double a, double b, double h)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Create, FileAccess.Write);
+            BinaryWriter bw = new BinaryWriter(fs);
+            double x = a;
+            while (x <= b)
+            {
+                bw.Write(delegFunc(x));
+                x += h;// x=x+h;
+            }
+            bw.Close();
+            fs.Close();
+        }
+
+        public static double Load(string fileName, out double minFunc)
+        {
+            FileStream fs = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            BinaryReader bw = new BinaryReader(fs);
+            double min = double.MaxValue;
+            double d;
+            for (int i = 0; i < fs.Length / sizeof(double); i++)
+            {
+                // Считываем значение и переходим к следующему
+                d = bw.ReadDouble();
+                if (d < min) min = d;
+            }
+            bw.Close();
+            fs.Close();
+            minFunc = min; //возвращает минимум функции через параметр(с использованием модификатора out).
+            return min;
+        }
+
+        static void Main(string[] args)
+        {
+            // Использовать массив (или список) делегатов, в котором хранятся различные функции.
+            Fn[] functions = new Fn[3];
+
+            functions[0] = new Fn(F1);
+            functions[1] = new Fn(F2);
+            functions[2] = new Fn(F3);
+
+            Console.WriteLine($"*********** Программа нахождения минимума функции  ***********");            
+            Console.WriteLine($"1) x^2-50*x-10");
+            Console.WriteLine($"2) x^2+4*x-x");
+            Console.WriteLine($"3) x^2-cos(x)\n");
+            Console.Write($"Выберите функцию (1, 2 или 3):");
+
+            int.TryParse(Console.ReadLine(), out int userInput);
+
+            if(userInput < 1 || userInput > 3)
+            {
+                Console.WriteLine($"Некорректный выбор! Выход...");
+                Console.ReadKey();
+                return;
+            } else
+            {
+                Console.WriteLine($"Выбрана функция номер {userInput}");
+            }
+
+            Console.WriteLine($"Введите начальное и конечное значение отрезка");
+            Console.Write($"a = ");
+            int.TryParse(Console.ReadLine(), out int a);
+            Console.Write($"b = ");
+            int.TryParse(Console.ReadLine(), out int b);
+
+            
+            SaveFunc("data.bin", functions[userInput-1], -100, 100, 0.5);
+            Console.WriteLine(Load("data.bin",out double functionMinimum));
+            Console.WriteLine($"Минимум функции: {functionMinimum}");
+            
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Lesson6/homework6/task3/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+// Студент: Дмитрий Фатеев
+
+/* Задание №3
+ * Переделать программу Пример использования коллекций для решения следующих задач:
+ * а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+ * б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся (*частотный массив);
+ * в) отсортировать список по возрасту студента;
+ * г) (*)отсортировать список по курсу и возрасту студента;
+ */
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] Courses = new int[6];
+        int courseFiveStudents = 0;
+        int courseSixStudents = 0;
+        // Создадим необобщенный список
+        ArrayList list = new ArrayList();
+        // Запомним время в начале обработки данных
+        DateTime dt = DateTime.Now;
+        StreamReader sr = new StreamReader("..\\..\\students_1.csv");
+        while (!sr.EndOfStream)
+        {
+            try
+            {
+                string[] s = sr.ReadLine().Split(';');
+                list.Add(s[1] + " " + s[0]);// Добавляем склееные имя и фамилию
+                int age = int.Parse(s[5]);
+                int course = int.Parse(s[6]);
+
+                // а) Подсчитать количество студентов учащихся на 5 и 6 курсах;
+                if (course == 5)
+                {
+                    courseFiveStudents++;
+                }
+                else if (course == 6)
+                {
+                    courseSixStudents++;
+                }
+
+                // б) подсчитать сколько студентов в возрасте от 18 до 20 лет на каком курсе учатся (*частотный массив);
+                if (age >= 18 && age <= 20)
+                {
+                    Courses[course-1]++;
+                }                
+            }
+            catch
+            {
+            }
+        }
+        sr.Close();
+        list.Sort();
+        Console.WriteLine($"Всего студентов:{list.Count}");
+        Console.WriteLine($"Студентов 5го курса:{courseFiveStudents}");
+        Console.WriteLine($"Студентов 6го курса:{courseSixStudents}");
+        Console.WriteLine($"Студенты в возрасте от 18 до 20 лет по курсам:");
+        Console.WriteLine($"1й курс: {Courses[0]}");
+        Console.WriteLine($"2й курс: {Courses[1]}");
+        Console.WriteLine($"3й курс: {Courses[2]}");
+        Console.WriteLine($"4й курс: {Courses[3]}");
+        Console.WriteLine($"5й курс: {Courses[4]}");
+        Console.WriteLine($"6й курс: {Courses[5]}");
+
+        // Вычислим время обработки данных
+        Console.WriteLine(DateTime.Now - dt);
+        Console.ReadKey();
+    }
+}
