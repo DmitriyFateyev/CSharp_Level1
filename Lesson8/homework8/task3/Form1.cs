de30a64b8e6a7e01670ee26b05de64459af04414
Dmitriy Fateyev <dmitriy.fateyev86@gmail.com>
2020-05-29 21:50:59 +0400

    homework8

---
+++ Lesson8/homework8/task3/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Windows.Forms;
+namespace BelieveOrNotBelieve
+{
+    public partial class Form1 : Form
+    {
+        // База данных с вопросами
+        TrueFalse database;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MiOpen_Click_1(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(ofd.FileName);
+                database.Load();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = database.Count;
+                nudNumber.Value = 1;
+            }
+        }
+
+        private void MiNew_Click_1(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(sfd.FileName);
+                database.Add("123", true);
+                database.Save();
+                nudNumber.Minimum = 1;
+                nudNumber.Maximum = 1;
+                nudNumber.Value = 1;
+            };
+        }
+
+        private void MiExit_Click_1(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void BtnSaveQuest_Click_1(object sender, EventArgs e)
+        {
+            database[(int)nudNumber.Value - 1].text = tboxQuestion.Text;
+            database[(int)nudNumber.Value - 1].trueFalse = cboxTrue.Checked;
+        }
+
+        private void MiSave_Click_1(object sender, EventArgs e)
+        {
+            if (database != null) database.Save();
+            else MessageBox.Show("База данных не создана");
+        }
+
+        private void BtnDelete_Click_1(object sender, EventArgs e)
+        {
+            if (nudNumber.Maximum == 1 || database == null) return;
+            database.Remove((int)nudNumber.Value);
+            nudNumber.Maximum--;
+            if (nudNumber.Value > 1) nudNumber.Value = nudNumber.Value;
+        }
+
+        private void BtnAdd_Click_1(object sender, EventArgs e)
+        {
+            if (database == null)
+            {
+                MessageBox.Show("Создайте новую базу данных", "Сообщение");
+                return;
+            }
+            database.Add((database.Count + 1).ToString(), true);
+            nudNumber.Maximum = database.Count;
+            nudNumber.Value = database.Count;
+        }
+
+        private void NudNumber_ValueChanged_1(object sender, EventArgs e)
+        {
+            tboxQuestion.Text = database[(int)nudNumber.Value - 1].text;
+            cboxTrue.Checked = database[(int)nudNumber.Value - 1].trueFalse;
+        }
+
+        private void MiSaveAs_Click(object sender, EventArgs e)
+        {
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                database = new TrueFalse(saveFileDialog1.FileName);
+                database.Add("123", true);
+                database.Save();
+            }
+        }
+    }
+}
